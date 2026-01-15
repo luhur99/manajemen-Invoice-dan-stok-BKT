4503eab63590495219877e356d8dbc500a4390b3
[elmony] <luhur@budikaryateknologi.com>
2026-01-15 22:39:56 +0700

    [elmony] Mengganti halaman utama dengan DashboardOverviewPage untuk menampilkan dashboard yang lebih fungsional. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import SidebarNav from "./components/SidebarNav";
-import Index from "./pages/Index";
+import DashboardOverviewPage from "./pages/DashboardOverviewPage"; // Mengimpor DashboardOverviewPage
 import SchedulingRequestManagementPage from "./pages/SchedulingRequestManagementPage";
 import InvoiceManagementPage from "./pages/InvoiceManagementPage";
 import PurchaseRequestManagementPage from "./pages/PurchaseRequestManagementPage";
@@ -14,12 +14,12 @@ import TechnicianScheduleCalendar from "./pages/TechnicianScheduleCalendar";
 import StockLedgerPage from "./pages/StockLedgerPage";
 import WarehouseInventoryPage from "./pages/WarehouseInventoryPage";
 import SalesDetailsPage from "./pages/SalesDetailsPage";
-import SettingsPage from "./pages/SettingsPage"; // New import
-import ProductManagementPage from "./pages/ProductManagementPage"; // New import
-import SupplierManagementPage from "./pages/SupplierManagementPage"; // New import
-import CustomerManagementPage from "./pages/CustomerManagementPage"; // New import
-import TechnicianManagementPage from "./pages/TechnicianManagementPage"; // New import
-import WarehouseCategoryManagementPage from "./pages/WarehouseCategoryManagementPage"; // New import
+import SettingsPage from "./pages/SettingsPage";
+import ProductManagementPage from "./pages/ProductManagementPage";
+import SupplierManagementPage from "./pages/SupplierManagementPage";
+import CustomerManagementPage from "./pages/CustomerManagementPage";
+import TechnicianManagementPage from "./pages/TechnicianManagementPage";
+import WarehouseCategoryManagementPage from "./pages/WarehouseCategoryManagementPage";
 
 const queryClient = new QueryClient();
 
@@ -33,7 +33,7 @@ function App() {
             <SidebarNav />
             <main className="flex-1 overflow-y-auto">
               <Routes>
-                <Route path="/" element={<Index />} />
+                <Route path="/" element={<DashboardOverviewPage />} /> {/* Menggunakan DashboardOverviewPage */}
                 <Route path="/scheduling-requests" element={<SchedulingRequestManagementPage />} />
                 <Route path="/invoices" element={<InvoiceManagementPage />} />
                 <Route path="/purchase-requests" element={<PurchaseRequestManagementPage />} />
@@ -42,12 +42,12 @@ function App() {
                 <Route path="/stock-ledger" element={<StockLedgerPage />} />
                 <Route path="/warehouse-inventory" element={<WarehouseInventoryPage />} />
                 <Route path="/sales-details" element={<SalesDetailsPage />} />
-                <Route path="/settings" element={<SettingsPage />} /> {/* New route */}
-                <Route path="/product-management" element={<ProductManagementPage />} /> {/* New route */}
-                <Route path="/supplier-management" element={<SupplierManagementPage />} /> {/* New route */}
-                <Route path="/customer-management" element={<CustomerManagementPage />} /> {/* New route */}
-                <Route path="/technician-management" element={<TechnicianManagementPage />} /> {/* New route */}
-                <Route path="/warehouse-category-management" element={<WarehouseCategoryManagementPage />} /> {/* New route */}
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/product-management" element={<ProductManagementPage />} />
+                <Route path="/supplier-management" element={<SupplierManagementPage />} />
+                <Route path="/customer-management" element={<CustomerManagementPage />} />
+                <Route path="/technician-management" element={<TechnicianManagementPage />} />
+                <Route path="/warehouse-category-management" element={<WarehouseCategoryManagementPage />} />
               </Routes>
             </main>
           </div>
